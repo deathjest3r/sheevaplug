9c9edc91f83588e4154de843907c7a626f782258
jester <jester@thinky.(none)>
2012-01-11 17:08:26 +0100

    Fixed the kernel panic druing boot

--- l4linux/arch/l4/kernel/arch-arm/mach_setup.c
+++ l4linux/arch/l4/kernel/arch-arm/mach_setup.c
@@ -7,13 +7,10 @@
 #include <linux/smsc911x.h>
 #include <linux/slab.h>
 #include <linux/list.h>
-#include <linux/mv643xx_eth.h>
 
 #include <asm/generic/devs.h>
 
 #include <asm/l4x/dma.h>
-
-#include "common.h"
 
 static int dev_init_done;
 
@@ -225,12 +222,133 @@ static L4X_DEVICE_CB(dmamem_cb)
 }
 
 //--- Start Sheevaplug Code (Julian)
-#include <mach/bridge-regs.h>
+#include <linux/mv643xx_eth.h>
 
-unsigned int kirkwood_clk_ctrl = CGC_DUNIT | CGC_RESERVED;
+#define KIRKWOOD_REGS_PHYS_BASE		0xf1000000
+#define KIRKWOOD_REGS_VIRT_BASE		0xfed00000
+
+#define GE00_PHYS_BASE		        (KIRKWOOD_REGS_PHYS_BASE | 0x70000)
+#define BRIDGE_VIRT_BASE	        (KIRKWOOD_REGS_VIRT_BASE | 0x20000)
+
+#define DDR_VIRT_BASE	            (KIRKWOOD_REGS_VIRT_BASE | 0x00000)
+#define  DDR_WINDOW_CPU_BASE    	(DDR_VIRT_BASE | 0x1500)
+
 struct mbus_dram_target_info kirkwood_mbus_dram_info;
-int kirkwood_tclk;
 
+//Initializierung des MBUS
+#define TARGET_DDR	        0
+#define TARGET_DEV_BUS		1
+#define TARGET_SRAM	    	3
+#define TARGET_PCIE		    4
+#define ATTR_DEV_NAND		0x2f
+#define ATTR_PCIE_IO		0xe0
+#define ATTR_PCIE_MEM		0xe8
+#define ATTR_PCIE1_IO		0xd0
+#define ATTR_PCIE1_MEM		0xd8
+#define ATTR_SRAM		    0x01
+
+#define DDR_BASE_CS_OFF(n)	(0x0000 + ((n) << 3))
+#define DDR_SIZE_CS_OFF(n)	(0x0004 + ((n) << 3))
+
+#define WIN_OFF(n)		    (BRIDGE_VIRT_BASE + 0x0000 + ((n) << 4))
+#define WIN_CTRL_OFF	    0x0000
+#define WIN_BASE_OFF		0x0004
+#define WIN_REMAP_LO_OFF    0x0008
+#define WIN_REMAP_HI_OFF	0x000c
+
+static int __init cpu_win_can_remap(int win)
+{
+	if (win < 4)
+		return 1;
+
+	return 0;
+}
+
+static void __init setup_cpu_win(int win, u32 base, u32 size,
+				 u8 target, u8 attr, int remap)
+{
+	void __iomem *addr = (void __iomem *)WIN_OFF(win);
+	u32 ctrl;
+
+	base &= 0xffff0000;
+	ctrl = ((size - 1) & 0xffff0000) | (attr << 8) | (target << 4) | 1;
+
+	writel(base, addr + WIN_BASE_OFF);
+	writel(ctrl, addr + WIN_CTRL_OFF);
+	if (cpu_win_can_remap(win)) {
+		if (remap < 0)
+			remap = base;
+
+		writel(remap & 0xffff0000, addr + WIN_REMAP_LO_OFF);
+		writel(0, addr + WIN_REMAP_HI_OFF);
+	}
+}
+
+void __init kirkwood_setup_cpu_mbus(void)
+{
+	void __iomem *addr;
+	int i;
+	int cs;
+
+	/*
+	 * First, disable and clear windows.
+	 */
+	for (i = 0; i < 8; i++) {
+		addr = (void __iomem *)WIN_OFF(i);
+
+        printk("Addr: 0x%x\n", WIN_OFF(i));
+
+		writel(0, addr + WIN_BASE_OFF);
+		writel(0, addr + WIN_CTRL_OFF);
+		if (cpu_win_can_remap(i)) {
+			writel(0, addr + WIN_REMAP_LO_OFF);
+			writel(0, addr + WIN_REMAP_HI_OFF);
+		}
+	}
+
+	/*setup_cpu_win(0, KIRKWOOD_PCIE_IO_PHYS_BASE, KIRKWOOD_PCIE_IO_SIZE,
+		      TARGET_PCIE, ATTR_PCIE_IO, KIRKWOOD_PCIE_IO_BUS_BASE);
+	setup_cpu_win(1, KIRKWOOD_PCIE_MEM_PHYS_BASE, KIRKWOOD_PCIE_MEM_SIZE,
+		      TARGET_PCIE, ATTR_PCIE_MEM, KIRKWOOD_PCIE_MEM_BUS_BASE);
+	setup_cpu_win(2, KIRKWOOD_PCIE1_IO_PHYS_BASE, KIRKWOOD_PCIE1_IO_SIZE,
+		      TARGET_PCIE, ATTR_PCIE1_IO, KIRKWOOD_PCIE1_IO_BUS_BASE);
+	setup_cpu_win(3, KIRKWOOD_PCIE1_MEM_PHYS_BASE, KIRKWOOD_PCIE1_MEM_SIZE,
+		      TARGET_PCIE, ATTR_PCIE1_MEM, KIRKWOOD_PCIE1_MEM_BUS_BASE);
+
+	setup_cpu_win(4, KIRKWOOD_NAND_MEM_PHYS_BASE, KIRKWOOD_NAND_MEM_SIZE,
+		      TARGET_DEV_BUS, ATTR_DEV_NAND, -1);
+
+	setup_cpu_win(5, KIRKWOOD_SRAM_PHYS_BASE, KIRKWOOD_SRAM_SIZE,
+		      TARGET_SRAM, ATTR_SRAM, -1);*/
+
+	/*
+	 * Setup MBUS dram target info.
+	 */
+	kirkwood_mbus_dram_info.mbus_dram_target_id = TARGET_DDR;
+
+	addr = (void __iomem *)DDR_WINDOW_CPU_BASE;
+
+	for (i = 0, cs = 0; i < 4; i++) {
+		u32 base = readl(addr + DDR_BASE_CS_OFF(i));
+		u32 size = readl(addr + DDR_SIZE_CS_OFF(i));
+
+		/*
+		 * Chip select enabled?
+		 */
+		if (size & 1) {
+			struct mbus_dram_window *w;
+
+			w = &kirkwood_mbus_dram_info.cs[cs++];
+			w->cs_index = i;
+			w->mbus_attr = 0xf & ~(1 << i);
+			w->base = base & 0xffff0000;
+			w->size = (size | 0x0000ffff) + 1;
+		}
+	}
+	kirkwood_mbus_dram_info.num_cs = cs;
+}
+
+// Ethernet Kram
 static __init void ge_complete(
 	struct mv643xx_eth_shared_platform_data *orion_ge_shared_data,
 	struct mbus_dram_target_info *mbus_dram_info, int tclk,
@@ -321,11 +439,8 @@ void __init orion_ge00_init(struct mv643xx_eth_platform_data *eth_data,
 
 void __init kirkwood_ge00_init(struct mv643xx_eth_platform_data *eth_data)
 {
-	kirkwood_clk_ctrl |= CGC_GE0;
-
 	orion_ge00_init(eth_data, &kirkwood_mbus_dram_info,
-			GE00_PHYS_BASE, IRQ_KIRKWOOD_GE00_SUM,
-			IRQ_KIRKWOOD_GE00_ERR, kirkwood_tclk);
+			GE00_PHYS_BASE, 11, 46, 200000000);
 }
 
 static struct mv643xx_eth_platform_data sheevaplug_ge00_data = {
@@ -339,6 +454,7 @@ static void register_platform_callbacks(void)
 	l4x_register_platform_device_callback("aaci",         aaci_cb);
 	l4x_register_platform_device_callback("dmamem",       dmamem_cb);
 
+    //kirkwood_setup_cpu_mbus();
     kirkwood_ge00_init(&sheevaplug_ge00_data);
     //--- End Sheevaplug Code (Julian)---
 }
