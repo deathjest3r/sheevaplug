6c05362d755c4d3c6233386fa3eaa2068b816eaf
jester <jester@jester>
2012-02-02 16:36:46 +0100

    Changed mach_setup back to its initial state

--- l4linux/arch/l4/kernel/arch-arm/mach_setup.c
+++ l4linux/arch/l4/kernel/arch-arm/mach_setup.c
@@ -221,125 +221,12 @@ static L4X_DEVICE_CB(dmamem_cb)
 		printk("Adding DMA memory to DMA allocator failed!\n");
 }
 
-//--- Start Sheevaplug Code (Julian)
-#include <linux/mv643xx_eth.h>
-
-#define KIRKWOOD_REGS_PHYS_BASE		0xf1000000
-#define KIRKWOOD_REGS_VIRT_BASE		0xfed00000
-
-#define GE00_PHYS_BASE		        (KIRKWOOD_REGS_PHYS_BASE | 0x70000)
-
-struct mbus_dram_target_info kirkwood_mbus_dram_info;
-
-static __init void ge_complete(
-	struct mv643xx_eth_shared_platform_data *orion_ge_shared_data,
-	struct mbus_dram_target_info *mbus_dram_info, int tclk,
-	struct resource *orion_ge_resource, unsigned long irq,
-	struct platform_device *orion_ge_shared,
-	struct mv643xx_eth_platform_data *eth_data,
-	struct platform_device *orion_ge)
-{
-	orion_ge_shared_data->dram = mbus_dram_info;
-	orion_ge_shared_data->t_clk = tclk;
-	orion_ge_resource->start = irq;
-	orion_ge_resource->end = irq;
-	eth_data->shared = orion_ge_shared;
-	orion_ge->dev.platform_data = eth_data;
-
-	platform_device_register(orion_ge_shared);
-	platform_device_register(orion_ge);
-}
-
-struct mv643xx_eth_shared_platform_data orion_ge00_shared_data;
-
-static struct resource orion_ge00_shared_resources[] = {
-	{
-		.name	= "ge00 base",
-	}, {
-		.name	= "ge00 err irq",
-	},
-};
-
-static struct platform_device orion_ge00_shared = {
-	.name		= MV643XX_ETH_SHARED_NAME,
-	.id		= 0,
-	.dev		= {
-		.platform_data	= &orion_ge00_shared_data,
-	},
-};
-
-static struct resource orion_ge00_resources[] = {
-	{
-		.name	= "ge00 irq",
-		.flags	= IORESOURCE_IRQ,
-	},
-};
-
-static struct platform_device orion_ge00 = {
-	.name		= MV643XX_ETH_NAME,
-	.id		= 0,
-	.num_resources	= 1,
-	.resource	= orion_ge00_resources,
-	.dev		= {
-		.coherent_dma_mask	= DMA_BIT_MASK(32),
-	},
-};
-
-static void fill_resources(struct platform_device *device,
-			   struct resource *resources,
-			   resource_size_t mapbase,
-			   resource_size_t size,
-			   unsigned int irq)
-{
-	device->resource = resources;
-	device->num_resources = 1;
-	resources[0].flags = IORESOURCE_MEM;
-	resources[0].start = mapbase;
-	resources[0].end = mapbase + size;
-
-	if (irq != NO_IRQ) {
-		device->num_resources++;
-		resources[1].flags = IORESOURCE_IRQ;
-		resources[1].start = irq;
-		resources[1].end = irq;
-	}
-}
-
-void __init orion_ge00_init(struct mv643xx_eth_platform_data *eth_data,
-			    struct mbus_dram_target_info *mbus_dram_info,
-			    unsigned long mapbase,
-			    unsigned long irq,
-			    unsigned long irq_err,
-			    int tclk)
-{
-	fill_resources(&orion_ge00_shared, orion_ge00_shared_resources,
-		       mapbase + 0x2000, SZ_16K - 1, irq_err);
-	ge_complete(&orion_ge00_shared_data, mbus_dram_info, tclk,
-		    orion_ge00_resources, irq, &orion_ge00_shared,
-		    eth_data, &orion_ge00);
-}
-
-void __init kirkwood_ge00_init(struct mv643xx_eth_platform_data *eth_data)
-{
-	orion_ge00_init(eth_data, &kirkwood_mbus_dram_info,
-			GE00_PHYS_BASE, 11, 46, 200000000);
-}
-
-static struct mv643xx_eth_platform_data sheevaplug_ge00_data = {
-    .phy_addr	= MV643XX_ETH_PHY_ADDR(0),
-};
-
 static void register_platform_callbacks(void)
 {
-
 	l4x_register_platform_device_callback("compactflash", realview_device_cb_pata);
 	l4x_register_platform_device_callback("smsc911x",     realview_device_cb_smsc);
 	l4x_register_platform_device_callback("aaci",         aaci_cb);
 	l4x_register_platform_device_callback("dmamem",       dmamem_cb);
-    //l4x_register_platform_device_callback("mv643xx",      kirkwood_device_cb_mv643xx);
-
-    kirkwood_ge00_init(&sheevaplug_ge00_data);
-    //--- End Sheevaplug Code (Julian)---
 }
 
 static void
